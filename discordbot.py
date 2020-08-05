189a0b5fa21af21c1e2ff00c05bba4415a3b8d23
rynaly <69234370+rynaly@users.noreply.github.com>
2020-08-06 00:23:29 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -31,7 +31,27 @@ async def bye(ctx):
     print('#切断')
     await ctx.voice_client.disconnect()
     await ctx.send('さよなら')
+    
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
 
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+
+    await ctx.message.attachments[0].save("output.wav")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("output.wav")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")
+    
 @client.event
 async def on_message(message):
     msgclient = message.guild.voice_client
@@ -42,7 +62,6 @@ async def on_message(message):
         if msgclient:
             print(message.content)
             #creat_WAV(message.content)
-            await ctx.message.attachments[0].save("output.wav")
             source = discord.FFmpegPCMAudio("output.wav")
             msgclient.play(source)
         else:
